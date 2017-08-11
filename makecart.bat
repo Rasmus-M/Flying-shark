539d596c984b62e801cd4b29fa2446e3475f462f
Rasmus-M <rasmus.moustgaard@gmail.com>
2017-08-11 18:41:45 +0200

    Cart

--- makecart.bat
+++ makecart.bat
@@ -1,5 +1,17 @@
+xas99.py -R -i source/shark.a99 -o shark
+@IF %ERRORLEVEL% NEQ 0 GOTO :end
+
+java -jar tools/ea5tocart.jar shark "FLYING SHARK" > make.log
+
 xas99.py -R -b source/level1-rom.a99 -o build/level1-rom
 tools\pad.exe build/level1-rom_6000_b0 build/level1_b0.bin 8192
 tools\pad.exe build/level1-rom_6000_b1 build/level1_b1.bin 8192
 tools\pad.exe build/level1-rom_6000_b2 build/level1_b2.bin 8192
 tools\pad.exe build/level1-rom_6000_b2 build/level1_b3.bin 8192
+copy /b shark8.bin + build\level1_b0.bin + build\level1_b1.bin + build\level1_b2.bin + build\level1_b3.bin flying-shark-8.bin
+
+WHERE jar
+@IF %ERRORLEVEL% NEQ 0 GOTO :end
+jar -cvf flying-shark.rpk flying-shark-8.bin layout.xml > make.log
+
+:end
