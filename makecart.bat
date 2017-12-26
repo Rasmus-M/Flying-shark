2758c93a95c02c4c6e72c4822f76615e05cef73f
Rasmus-M <rasmus.moustgaard@gmail.com>
2017-12-26 19:41:00 +0100

    Music on title screen

--- makecart.bat
+++ makecart.bat
@@ -3,6 +3,8 @@ xas99.py -R -i -S -L shark.lst source/shark.a99 -o shark
 
 java -jar tools/ea5tocart.jar shark "FLYING SHARK" > make.log
 
+tools\pad.exe sound/title.vgm.spf build/title-music.bin 7168
+
 copy /b ^
 shark8.bin + ^
 build\empty-2k.bin + build\TITLE.TIAP + ^
@@ -55,6 +57,7 @@ build\level4_b8.bin + ^
 build\level4_b9.bin + ^
 build\level4_b10.bin + ^
 build\empty-1k.bin + build\level4_b11.bin + ^
+build\empty-1k.bin + build\title-music.bin + ^
 build\empty.bin + ^
 build\empty.bin + ^
 build\empty.bin + ^
