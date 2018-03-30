2d8338777a066e0a5858329204ee39dd1a5d0b7b
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-03-30 16:31:42 +0200

    Landing music

--- makecart.bat
+++ makecart.bat
@@ -4,6 +4,7 @@ xas99.py -R -i -S -L shark.lst source/shark.a99 -o shark
 java -jar tools/ea5tocart.jar shark "FLYING SHARK" > make.log
 
 tools\pad.exe sound/title.vgm.spf build/title-music.bin 7168
+tools\pad.exe sound/landing.vgm.spf build/landing-music.bin 7168
 
 copy /b ^
 shark8.bin + ^
@@ -58,7 +59,7 @@ build\level4_b9.bin + ^
 build\level4_b10.bin + ^
 build\empty-1k.bin + build\level4_b11.bin + ^
 build\empty-1k.bin + build\title-music.bin + ^
-build\empty.bin + ^
+build\empty-1k.bin + build\landing-music.bin + ^
 build\empty.bin + ^
 build\empty.bin + ^
 build\empty.bin + ^
